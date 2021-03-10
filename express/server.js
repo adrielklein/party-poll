dc797317f38c18e1b064157f9c21607d10ede09a
Adriel Klein <adriel@level.com>
2021-03-10 17:49:03 -0500

    change order

--- express/server.js
+++ express/server.js
@@ -26,8 +26,8 @@ router.post("/", (req, res) => {
 });
 
 app.use(bodyParser.json());
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
